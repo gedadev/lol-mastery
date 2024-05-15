95786cf7cd3aff4e98d441b3d8b63e3a579b7465
Gedaias Jair <gedaias@geda.dev>
2024-05-14 22:17:22 -0600

    feat: Add pagination to display the champ list

--- src/views/Summoner.jsx
+++ src/views/Summoner.jsx
@@ -1,11 +1,20 @@
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import championFull from "../assets/data/championFull.json";
+import ChampImage from "./components/ChampImage";
 
 export default function Summoner() {
   const location = useLocation();
   const puuid = location.state.puuid;
   const [champList, setChampList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [champsPerPage, setChampsPerPage] = useState(30);
+  const totalPage = Math.ceil(champList.length / champsPerPage);
+  const sliceData = champList.slice(
+    (currentPage - 1) * champsPerPage,
+    currentPage * champsPerPage
+  );
 
   useEffect(() => {
     const getChampList = async () => {
@@ -22,5 +31,33 @@ export default function Summoner() {
     getChampList();
   }, [puuid]);
 
-  return <div></div>;
+  const getChampData = (champId) => {
+    const champData = Object.values(championFull.data).find(
+      (champ) => champ.key === champId
+    );
+
+    return {
+      name: champData.name,
+      title: champData.title,
+      image: champData.image.full,
+    };
+  };
+
+  return (
+    <section className="p-2">
+      <h1>Summoner info</h1>
+      <div className="m-auto grid grid-cols-5 gap-2 md:max-w-screen-lg">
+        {sliceData.map((champ) => (
+          <article key={champ.championId}>
+            <ChampImage name={getChampData(String(champ.championId)).image} />
+            <h2>{getChampData(String(champ.championId)).name}</h2>
+            <p>{champ.championLevel}</p>
+            <p>{champ.championPoints}</p>
+            {champ.chestGranted ? <p>Chest Granted</p> : <p>No Chest</p>}
+            <p>{champ.lastPlayTime}</p>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
 }
