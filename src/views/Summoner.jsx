edc5c5221b24afba2ec4e6df53876fd82f963356
Gedaias Jair <gedaias@geda.dev>
2024-05-14 18:36:18 -0600

    Add champion list retrieval for Summoner view

--- src/views/Summoner.jsx
+++ src/views/Summoner.jsx
@@ -1,8 +1,26 @@
 import { useLocation } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Summoner() {
   const location = useLocation();
   const puuid = location.state.puuid;
+  const [champList, setChampList] = useState([]);
 
-  return <p></p>;
+  useEffect(() => {
+    const getChampList = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/getChampList?puuid=${puuid}`
+        );
+        setChampList(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getChampList();
+  }, [puuid]);
+
+  return <div></div>;
 }
