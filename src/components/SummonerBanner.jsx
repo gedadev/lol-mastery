924fcf4a0fd4f03b9782f78c9032f1c0810669f8
Gedaias Jair <gedaias@geda.dev>
2024-07-10 21:03:09 -0600

    feat: add skeleton loader for the banner

--- src/components/SummonerBanner.jsx
+++ src/components/SummonerBanner.jsx
@@ -1,73 +1,46 @@
-import PropTypes from "prop-types";
-import { useContext, useEffect, useState } from "react";
-import axios from "axios";
+import { useContext } from "react";
 import { convertTimestampToDate } from "../utils/epochConverter";
-import { APIContext } from "../context/APIContext";
-import useSummonerData from "../hooks/useSummonerData";
+import { SummonerContext } from "../context/SummonerContext";
+import { Skeleton } from "@mui/material";
 
-export default function SummonerBanner({ gameName, tagLine, puuid }) {
-  const [summonerInfo, setSummonerInfo] = useState({
-    gameName: gameName,
-    tagLine: tagLine,
-    profileIconId: null,
-    revisionDate: null,
-    summonerLevel: null,
-  });
-  const { serverURL } = useContext(APIContext);
-  const { summonerData } = useSummonerData();
-
-  useEffect(() => {
-    const getIcon = async () => {
-      try {
-        const summonerData = await axios.get(
-          `${serverURL}/getSummonerInfo?puuid=${puuid}`
-        );
-        setSummonerInfo((info) => ({
-          ...info,
-          profileIconId: summonerData.data.profileIconId,
-          revisionDate: summonerData.data.revisionDate,
-          summonerLevel: summonerData.data.summonerLevel,
-        }));
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    getIcon();
-  }, [puuid, serverURL]);
-
-  useEffect(() => {
-    console.log(summonerData);
-  }, [summonerData]);
+export default function SummonerBanner() {
+  const { summonerData } = useContext(SummonerContext);
 
   return (
-    <section className="mx-auto my-6 px-5 md:max-w-screen-xl flex gap-5 justify-between sm:justify-start">
-      {console.log(summonerData)}
-      <div className="relative">
-        <img
-          src={`https://ddragon.leagueoflegends.com/cdn/14.10.1/img/profileicon/${summonerInfo.profileIconId}.png`}
-          alt={`${gameName}-icon`}
-          className="max-w-28 rounded-full"
-        />
-        <div className="bg-red-800 rounded-full shadow-md shadow-stone-500 w-10 h-8 flex items-center justify-center absolute bottom-0 left-1/2 -ml-5 -mb-4">
-          {summonerInfo.summonerLevel}
+    <>
+      {summonerData.puuid ? (
+        <section className="mx-auto my-6 px-5 md:max-w-screen-xl flex gap-5 justify-between sm:justify-start">
+          <div className="relative">
+            <img
+              src={`https://ddragon.leagueoflegends.com/cdn/14.10.1/img/profileicon/${summonerData.profileIconId}.png`}
+              alt={`${summonerData.gameName}-icon`}
+              className="max-w-28 rounded-full"
+            />
+            <div className="bg-red-800 rounded-full shadow-md shadow-stone-500 w-10 h-8 flex items-center justify-center absolute bottom-0 left-1/2 -ml-5 -mb-4">
+              {summonerData.summonerLevel}
+            </div>
+          </div>
+          <div className="self-center">
+            <div className="sm:flex items-end gap-2">
+              <h1 className="text-2xl sm:text-3xl">{summonerData.gameName}</h1>
+              <h2>#{summonerData.tagLine}</h2>
+            </div>
+            <p className="opacity-50 col-span-2">
+              Last update: {convertTimestampToDate(summonerData.revisionDate)}
+            </p>
+          </div>
+        </section>
+      ) : (
+        <div className="p-5 flex items-center gap-5">
+          <Skeleton
+            variant="circular"
+            animation="wave"
+            width={110}
+            height={110}
+          />
+          <Skeleton variant="text" animation="wave" width={250} height={100} />
         </div>
-      </div>
-      <div className="self-center">
-        <div className="sm:flex items-end gap-2">
-          <h1 className="text-2xl sm:text-3xl">{gameName}</h1>
-          <h2>#{tagLine}</h2>
-        </div>
-        <p className="opacity-50 col-span-2">
-          Last update: {convertTimestampToDate(summonerInfo.revisionDate)}
-        </p>
-      </div>
-    </section>
+      )}
+    </>
   );
 }
-
-SummonerBanner.propTypes = {
-  gameName: PropTypes.string.isRequired,
-  tagLine: PropTypes.string.isRequired,
-  puuid: PropTypes.string.isRequired,
-};
